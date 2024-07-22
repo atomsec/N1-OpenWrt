c1cb1e533ab2cdbbbbe11beb5597630f029f0e19
atomsec <32327666+atomsec@users.noreply.github.com>
2024-07-22 23:31:15 +0900

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -7,11 +7,3 @@ sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generat
 rm -rf feeds/luci/applications/luci-app-passwall
 
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
-git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
